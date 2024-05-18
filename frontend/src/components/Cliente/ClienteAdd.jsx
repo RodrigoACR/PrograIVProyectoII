d9f6925120d86e9c600ca3ebffa0952269178f02
RodrigoACR <rodrigo.castro.rojas@est.una.ac.cr>
2024-05-18 12:34:53 -0600

    Cliente Add Page

--- frontend/src/components/Cliente/ClienteAdd.jsx
+++ frontend/src/components/Cliente/ClienteAdd.jsx
@@ -1,7 +1,73 @@
+import {useState} from "react";
+
 const ClienteAdd = () => {
+
+    const [nombre, setNombre] = useState('')
+    const [tipoIdentificacion, setTipoIdentificacion] = useState('')
+    const [numeroIdentificacion, setNumeroIdentificacion] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [correoelectronico, setCorreoelectronico] = useState('')
+    const [idemisor, setIdemisor] = useState('')
+
+    function guardarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre,
+            tipoIdentificacion,
+            numeroIdentificacion,
+            telefono,
+            correoelectronico,
+            idemisor
+        }
+
+        console.log(cliente);
+    }
+
     return (
         <div className={"container"}>
-            <h1>Nuevo Cliente</h1>
+            <br/>
+            <br/>
+            <div className={"card col-md-6 offset-3"}>
+                <h1 className={"text-center card-header"}>Nuevo Cliente</h1>
+                <div className={"card-body"}>
+                    <form>
+                        <div className={"mb-3"}>
+                            <label htmlFor="nombre" className={"form-label"}>Nombre</label>
+                            <input type="text" id="nombre" className={"form-control"} value={nombre}
+                                   onChange={(e) => setNombre(e.target.value)}/>
+                        </div>
+                        <div className={"mb-3"}>
+                            <label htmlFor="tipoIdentificacion" className={"form-label"}>Tipo de Identificación</label>
+                            <input type="text" id="tipoIdentificacion" className={"form-control"}
+                                   value={tipoIdentificacion} onChange={(e) => setTipoIdentificacion(e.target.value)}/>
+                        </div>
+                        <div className={"mb-3"}>
+                            <label htmlFor="numeroIdentificacion" className={"form-label"}>Número de
+                                Identificación</label>
+                            <input type="text" id="numeroIdentificacion" className={"form-control"}
+                                   value={numeroIdentificacion}
+                                   onChange={(e) => setNumeroIdentificacion(e.target.value)}/>
+                        </div>
+                        <div className={"mb-3"}>
+                            <label htmlFor="telefono" className={"form-label"}>Teléfono</label>
+                            <input type="text" id="telefono" className={"form-control"} value={telefono}
+                                   onChange={(e) => setTelefono(e.target.value)}/>
+                        </div>
+                        <div className={"mb-3"}>
+                            <label htmlFor="correoelectronico" className={"form-label"}>Correo Electrónico</label>
+                            <input type="text" id="correoelectronico" className={"form-control"}
+                                   value={correoelectronico} onChange={(e) => setCorreoelectronico(e.target.value)}/>
+                        </div>
+                        <div className={"mb-3"}>
+                            <label htmlFor="idemisor" className={"form-label"}>ID Emisor</label>
+                            <input type="text" id="idemisor" className={"form-control"} value={idemisor}
+                                   onChange={(e) => setIdemisor(e.target.value)}/>
+                        </div>
+                        <button className={"btn btn-success"} onClick={guardarCliente}>Guardar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className={"fixed-bottom"}>
+        <div className={"align-bottom"}>
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="d-flex align-items-center">
                     <span className="text-body-secondary">© 2024 Programacion IV, Proyecto II</span>
