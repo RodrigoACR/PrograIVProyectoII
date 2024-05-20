4d60b0f416ea75d212e8257318930652e422b7b9
RodrigoACR <rodrigo.castro.rojas@est.una.ac.cr>
2024-05-20 15:19:31 -0600

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,11 +2,32 @@ const Header = () => {
     return (
         <div>
             <header>
-                <nav className={"navbar navbar-dark bg-primary"}>
-                    <a href="/">Facturas</a>
-                    <a href="/">Productos</a>
-                    <a href="/clientes">Clienes</a>
-                    <a href="/">Salir</a>
+                <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                    <div className="container-fluid d-flex flex-column flex-md-row">
+                        <a className="navbar-brand" href="/main">Inicio</a>
+                        <div className={"navbar-collapse collapse show"}>
+                            <ul className="navbar-nav me-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="/facturas">Facturas
+                                        <span className="visually-hidden">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/clientes">Clientes</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/productos">Productos</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">About</a>
+                                </li>
+                            </ul>
+                            <div className="d-flex">
+                                <h4 className={"text-white fw-bold me-4"}>Bienvenido Usuario!</h4>
+                                <a className="btn btn-secondary my-2 my-sm-0">Salir</a>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
             </header>
         </div>
